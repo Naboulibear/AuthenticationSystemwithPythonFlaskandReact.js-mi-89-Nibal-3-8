0b6118da0e8e9e50351c1bfbb1cd7abb8f18412e
Naboulibear <108967590+Naboulibear@users.noreply.github.com>
2026-05-26 18:56:40 +0300

    Change login fetch URL from localhost:3001 to /api/login for proxy support

--- src/js/component/login.jsx
+++ src/js/component/login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
 		}
 
 		try {
-			const response = await fetch(`http://localhost:3001/api/login`, {
+			const response = await fetch(`/api/login`, {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
